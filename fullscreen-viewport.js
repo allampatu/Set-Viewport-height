1c7d8c03bca8b92e34e6bb39c7b54eb1af812ec5
Sergio Occhipinti <allampatu@users.noreply.github.com>
2016-04-03 13:40:27 +0200

    Create fullscreen-viewport.js file

---
+++ fullscreen-viewport.js
@@ -0,0 +1,13 @@
+getWidthAndHeight();
+
+// make sure div stays full width/height on resize
+jQuery(window).resize(function() {
+    getWidthAndHeight();
+});
+
+function getWidthAndHeight (){
+    var winWidth = jQuery(window).width();
+    var winHeight = jQuery(window).height();
+    var headerHeight = jQuery('header').height(); 
+    jQuery('.fullscreen').css({'width': winWidth,'height': winHeight - headerHeight,});
+}
